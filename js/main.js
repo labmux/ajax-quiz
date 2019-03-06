d6cc07d4c4db805ecfcab4c651ca8a7665353739
Eliran Assaraf <eliran.a.bg@gmail.com>
2019-03-06 12:18:58 -0500

    final version

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 //Run jQuery after the document is fully loaded.
 $(document).ready(
 
-    $('#searchBtn').bind('click', function () {
-        $('#searchBar').load('index.html', function (responseTxt,statusText,xhr) {
-            
-        })
-    }),
-
     //The function that does the stuff.
     function () {
         //Make the AJAX call
@@ -29,5 +23,36 @@ $(document).ready(
                             +' </tr>')
             })
         })
-    }
+    },
+
+    $('#search_btn').click(function () {
+        //get search query
+        var search_query = document.getElementById('search_query').value;
+        //replace spaces with +
+        search_query = search_query.replace(/\s+/g, '+');
+
+        let ajax_call = 'http://api.tvmaze.com/singlesearch/shows?q=' + search_query + '&embed=episodes';
+
+        //Make the AJAX call
+        $.ajax(ajax_call, {
+            method: "GET",
+            dataType: "json"
+        }).done(
+                function (data) {
+                    //Add the name
+                    $('#name').empty().append(data.name);
+                    //empty previous list
+                    $('#episodeList').empty();
+
+                    //display episodes
+                    data._embedded.episodes.forEach(function (episode) {
+                        $('#episodeList').append('<tr>'+
+                            '<td>' + episode.season + '</td>' +
+                            '<td>' + episode.number + '</td>' +
+                            '<td>' + episode.name + '</td>' +
+                            '<td>' + episode.summary + '</td>' +
+                            +' </tr>')
+                    })
+                })
+    })
 )
